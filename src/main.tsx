7345c38f9e64b8a918661c92172bc787d173ad75
EJ <yi.jie.lee890418@gmail.com>
2025-08-16 09:13:36 +0800

    修正main.tsx 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import FitnessTracker from './tracker';
+import './index.css';
 
 const root = document.getElementById('root');
 if (root) {
